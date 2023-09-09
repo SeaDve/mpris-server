f72f41659dfc9c11143611cb7d6549d818dc92a0
Dave Patrick Caberto <davecruz48@gmail.com>
2023-09-09 21:45:22 +0800

    add more in example

--- examples/player.rs
+++ examples/player.rs
@@ -5,11 +5,21 @@ async fn main() {
     let player = Player::builder("Test.Application")
         .can_play(true)
         .can_pause(true)
+        .can_go_previous(true)
+        .can_go_next(true)
         .build()
         .unwrap();
 
     player.connect_play_pause(|| {
         println!("PlayPause");
+    });
+
+    player.connect_previous(|| {
+        println!("Previous");
+    });
+
+    player.connect_next(|| {
+        println!("Next");
     });
 
     player.run().await.unwrap();
