83ae0596cbae2440539f0aa805879c1592f5fd8b
Dave Patrick Caberto <davecruz48@gmail.com>
2023-09-12 19:28:08 +0800

    document Playlist

--- src/playlist.rs
+++ src/playlist.rs
@@ -3,10 +3,16 @@ use zbus::zvariant::{ObjectPath, Type, Value};
 
 use crate::{PlaylistId, Uri};
 
+/// A data structure describing a playlist.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Type)]
 pub struct Playlist {
+    /// A unique identifier for the playlist.
+    ///
+    /// This should remain the same if the playlist is renamed.
     pub id: PlaylistId,
+    /// The name of the playlist, typically given by the user.
     pub name: String,
+    /// The URI of an (optional) icon.
     pub icon: Uri,
 }
 
@@ -16,9 +22,17 @@ impl<'a> From<Playlist> for Value<'a> {
     }
 }
 
+/// A data structure describing a playlist, or nothing.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Type)]
 pub struct MaybePlaylist {
+    /// Whether this structure refers to a valid playlist.
     valid: bool,
+    /// The playlist, providing `valid` is true, otherwise undefined.
+    ///
+    /// When constructing this type, it should be noted that the playlist ID
+    /// must be a valid object path, or D-Bus implementations may reject it.
+    /// This is true even when Valid is false. It is suggested that "/" is
+    /// used as the playlist ID in this case.
     playlist: Playlist,
 }
 
