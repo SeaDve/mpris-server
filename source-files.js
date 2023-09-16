9fd55326251d78049f34c12d98861247f970dff7
SeaDve <SeaDve@users.noreply.github.com>
2023-09-16 03:56:36 +0000

    deploy: 894b5ee6913952fefc0d098cdef7becc857d0e8b

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"mpris_server":["",[],["lib.rs","local_server.rs","loop_status.rs","metadata.rs","playback_status.rs","player.rs","playlist.rs","playlist_ordering.rs","property.rs","server.rs","time.rs","track_id.rs"]]\
+"mpris_server":["",[],["lib.rs","local_server.rs","loop_status.rs","metadata.rs","playback_status.rs","player.rs","playlist.rs","playlist_ordering.rs","property.rs","server.rs","signal.rs","time.rs","track_id.rs"]]\
 }');
 createSourceSidebar();
