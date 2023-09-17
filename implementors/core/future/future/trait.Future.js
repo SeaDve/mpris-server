7fa5329fd6501faaeea7e3441666a33886d2c3a8
SeaDve <SeaDve@users.noreply.github.com>
2023-09-17 08:18:28 +0000

    deploy: f36b48ea6a85bd8e28fd475f9b03737c30aa9660

---
+++ implementors/core/future/future/trait.Future.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mpris_server":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"mpris_server/struct.LocalServerTask.html\" title=\"struct mpris_server::LocalServerTask\">LocalServerTask</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
