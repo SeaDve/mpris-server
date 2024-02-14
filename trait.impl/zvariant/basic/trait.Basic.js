84c70be1c9dab80e582a11b1e653ffd133fcfd9c
SeaDve <SeaDve@users.noreply.github.com>
2024-02-14 13:07:52 +0000

    deploy: 94ef96c24e6d877de06501451deca4793d2bd90c

---
+++ trait.impl/zvariant/basic/trait.Basic.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mpris_server":[["impl Basic for <a class=\"struct\" href=\"mpris_server/struct.TrackId.html\" title=\"struct mpris_server::TrackId\">TrackId</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
